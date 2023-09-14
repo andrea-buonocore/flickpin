e5647defae1bb6d6df3cd70ba850a6f8f70f3b75
andrea-buonocore <andreabuonocore00@gmail.com>
2023-09-14 22:19:01 +0200

    edit profile position (center or left?)

--- src/pages/user_page/UserPage.tsx
+++ src/pages/user_page/UserPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { User } from "../../Interfaces/Photo";
+import { Photo, User } from "../../Interfaces/Photo";
 import { FaSpinner } from "react-icons/fa";
 import { FaXTwitter, FaInstagram, FaPaypal, FaDev } from "react-icons/fa6";
 
@@ -8,8 +8,8 @@ const UserPage = () => {
 
     const params = useParams();
     const [user, setUser] = useState<User | null>(null);
+    const [userPhotos, setUserPhotos] = useState<Photo[] | [] | null>([])
     const [isLoading, setIsLoading] = useState<boolean>(true);
-
     const { username } = params;
 
     const headersList = {
@@ -20,7 +20,7 @@ const UserPage = () => {
 
     useEffect(() => {
 
-        const getPhotos = async () => {
+        const getUserProfile = async () => {
 
             try {
 
@@ -43,12 +43,36 @@ const UserPage = () => {
             }
         }
 
-        getPhotos();
+        const getUserPhotos = async () => {
+
+            try {
+
+                let response = await fetch(`${user?.links.photos}`, {
+                    method: "GET",
+                    headers: headersList
+                });
+
+                let data = await response.json();
+                console.log('foto', data);
+                setUserPhotos(data);
+
+
+            } catch (error) {
+                console.log(error);
+            }
+
+        }
+
+        getUserProfile();
+        getUserPhotos();
+
     }, []);
 
 
     return (
-        <div className="w-full h-full p-4 lg:p-8">
+        // main container
+        <div className="w-full h-full p-4 lg:p-8 text-center">
+
             {
                 isLoading && (
                     <div className="flex justify-center my-4">
@@ -56,6 +80,7 @@ const UserPage = () => {
                     </div>
                 )
             }
+
             {/* username & photo */}
             <div className="flex flex-col items-center gap-2 my-4">
                 <img
@@ -68,67 +93,89 @@ const UserPage = () => {
                 </div>
             </div>
 
-            {/* followers & download */}
+            {/* photos & likes */}
 
             <div className="flex items-center justify-around my-4">
                 <div className="flex flex-col items-center">
                     <div>
-                        {user?.followers_count}
+                        {user?.total_photos}
                     </div>
                     <div className="font-bold">
-                        followers
+                        photos
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
                     <div>
-                        {user?.downloads}
+                        {user?.total_likes}
                     </div>
                     <div className="font-bold">
-                        downloads
+                        likes
                     </div>
                 </div>
             </div>
 
-            {/* bio */}
-            <div className="my-4">
-                <div className="font-bold text-lg mb-4">About me:</div>
-                {user?.bio}
+            {/* bio & social grid container */}
+            <div className="md:grid md:grid-cols-2 gap-12">
+
+                {/* bio */}
+                <div className="my-4">
+                    <div className="font-bold text-lg mb-4">About me:</div>
+                    {user?.bio}
+                </div>
+
+                {/* social */}
+                <div className="my-4">
+                    <div className="font-bold text-lg mb-4">Social:</div>
+                    {
+                        user?.social.instagram_username &&
+                        <div className="flex items-center gap-4 mb-4">
+                            <FaInstagram size={40} />
+                            <a href={`https://www.instagram.com/${user?.social.instagram_username}`} target="_blank">{user?.social.instagram_username}</a>
+
+                        </div>
+                    }
+                    {
+                        user?.social.twitter_username &&
+                        <div className="flex items-center gap-4 mb-4">
+                            <FaXTwitter size={40} />
+                            <a href={`https://www.twitter.com/${user?.social.twitter_username}`} target="_blank">{user?.social.twitter_username}</a>
+
+                        </div>
+                    }
+                    {
+                        user?.social.paypal_email &&
+                        <div className="flex items-center gap-4 mb-4">
+                            <FaPaypal size={40} />
+                            {user?.social.paypal_email}
+                        </div>
+                    }
+                    {
+                        user?.social.portfolio_url &&
+                        <div className="flex items-center gap-4 mb-4">
+                            <FaDev size={40} />
+                            <a href={user?.portfolio_url} target="_blank">{user?.portfolio_url}</a>
+
+
+                        </div>
+                    }
+                </div>
+
             </div>
 
-            {/* social */}
+            {/* user photos */}
             <div className="my-4">
-                <div className="font-bold text-lg mb-4">Social:</div>
-                {
-                    user?.social.instagram_username &&
-                    <div className="flex items-center gap-4 mb-4">
-                        <FaInstagram size={40} />
-                        {user?.social.instagram_username}
-                    </div>
-                }
-                {
-                    user?.social.twitter_username &&
-                    <div className="flex items-center gap-4 mb-4">
-                        <FaXTwitter size={40} />
-                        {user?.social.twitter_username}
-                    </div>
-                }
-                {
-                    user?.social.paypal_email &&
-                    <div className="flex items-center gap-4 mb-4">
-                        <FaPaypal size={40} />
-                        {user?.social.paypal_email}
-                    </div>
-                }
-                {
-                    user?.social.portfolio_url &&
-                    <div className="flex items-center gap-4 mb-4">
-                        <FaDev size={40} />
-                        {user?.portfolio_url}
-                    </div>
-                }
+                <div className="font-bold text-lg mb-4">My Pics:</div>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+                    {
+                        userPhotos && userPhotos.map((photo) => {
+                            return (
+                                <img src={photo.urls.regular} alt={photo.alt_description} className="rounded-lg aspect-square object-cover" />
+                            )
+                        })
+                    }
+                </div>
+
             </div>
-
-
 
         </div>
     )
--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -3,10 +3,10 @@ const Footer = () => {
         <footer className="p-4 text-center text-gray-500 text-sm">
             <div className="flex justify-center gap-4 mb-2 text-xs">
                 <div>
-                    <a href="http://">Github Repo</a>
+                    <a href="https://github.com/andrea-buonocore" target="_blank">Github Repo</a>
                 </div>
                 <div>
-                    <a href="http://">Linkedin Profile</a>
+                    <a href="https://www.linkedin.com/in/andreabuonocore00/" target="_blank">Linkedin Profile</a>
                 </div>
             </div>
             <div>
